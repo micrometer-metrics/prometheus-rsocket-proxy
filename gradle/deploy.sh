c864ed82f5a3a259b745931c81dec1e5a74f8ce7
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2022-02-17 19:36:06 -0800

    Gradle 7 Migration (#48)
    * Upgrade Gradle to 7.4
    
    * Remove spring-release-plugin and add plugins to fix the build
    License plugin does not work since it uses spring framework 3.x: https://github.com/hierynomus/license-gradle-plugin/issues/161
    
    * Pin micrometer-registry-prometheus to 1.6.1 (the same version as boot uses)
    
    * Generate dependency lock files
    
    * circleci and deploy.sh
    
    * Get Docker creds from env vars

--- gradle/deploy.sh
+++ gradle/deploy.sh
@@ -1,31 +1,33 @@
-#!/bin/bash
+#!/bin/bash -e
 # This script will deploy the project artifacts.
 
-SWITCHES="-s --console=plain"
+SWITCHES="-s --console=plain -x test"
+# circleci does not like multi-line values so they are base64 encoded
+ORG_GRADLE_PROJECT_SIGNING_KEY="$(echo "$ORG_GRADLE_PROJECT_SIGNING_KEY" | base64 -d)"
 
 if [ $CIRCLE_PR_NUMBER ]; then
   echo -e "WARN: Should not be here => Found Pull Request #$CIRCLE_PR_NUMBER => Branch [$CIRCLE_BRANCH]"
   echo -e "Not attempting to publish"
 elif [ -z $CIRCLE_TAG ]; then
   echo -e "Publishing Snapshot => Branch ['$CIRCLE_BRANCH']"
-  openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 1000 -in gradle.properties.enc -out gradle.properties -pass "pass:$KEY"
-  ./gradlew snapshot bootBuildImage --publishImage $SWITCHES -x release -x test
+  ./gradlew -Prelease.stage=SNAPSHOT snapshot bootBuildImage publishNebulaPublicationToSnapshotRepository --publishImage $SWITCHES
 elif [ $CIRCLE_TAG ]; then
-  echo -e "Publishing Release => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']"
-  openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 1000 -in gradle.properties.enc -out gradle.properties -pass "pass:$KEY"
+  echo -e "Publishing Release => Branch ['$CIRCLE_BRANCH'] Tag ['$CIRCLE_TAG']"
   case "$CIRCLE_TAG" in
-  *-rc\.*)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true candidate bintrayUpload $SWITCHES -x release -x bintrayPublish
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true :prometheus-rsocket-client:bintrayPublish bootBuildImage --publishImage $SWITCHES -x bintrayUpload
+  *-M*)
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=milestone candidate bootBuildImage publishNebulaPublicationToMilestoneRepository --publishImage $SWITCHES
+    ;;
+  *-RC*)
+     # -Prelease.stage=milestone instead of rc (should be rc), probably related to this bug: https://github.com/nebula-plugins/nebula-release-plugin/issues/213
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=milestone candidate bootBuildImage publishNebulaPublicationToMilestoneRepository --publishImage $SWITCHES
     ;;
   *)
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true final bintrayUpload $SWITCHES -x release -x artifactoryPublish -x bintrayPublish
-    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true :prometheus-rsocket-client:bintrayPublish bootBuildImage --publishImage $SWITCHES -x bintrayUpload
-    ./gradlew bootBuildImage --imageName=micrometermetrics/prometheus-rsocket-proxy:latest --publishImage
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=final final bootBuildImage publishNebulaPublicationToMavenCentralRepository closeAndReleaseMavenCentralStagingRepository --publishImage $SWITCHES
+    ./gradlew -Prelease.disableGitChecks=true -Prelease.useLastTag=true -Prelease.stage=final final bootBuildImage --imageName=micrometermetrics/prometheus-rsocket-proxy:latest --publishImage $SWITCHES
     ;;
   esac
 else
-  echo -e "WARN: Should not be here => Branch ['$CIRCLE_BRANCH']  Tag ['$CIRCLE_TAG']  Pull Request ['$CIRCLE_PR_NUMBER']"
+  echo -e "WARN: Should not be here => Branch ['$CIRCLE_BRANCH'] Tag ['$CIRCLE_TAG'] Pull Request ['$CIRCLE_PR_NUMBER']"
   echo -e "Not attempting to publish"
 fi
 
